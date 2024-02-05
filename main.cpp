83522524f4e4bae9ae980d341e364bbc0719ce1a
Minh Hieu <95726917+Zeustakeshi@users.noreply.github.com>
2024-02-05 16:11:28 +0700

    Hello (#10)
    * Update main.cpp
    
    * update main.cpp
    
    * Update hello.txt
    
    * update
    
    * Update hello.txt
    
    * update hello.txt
    
    * update hello.txt

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main () {
-  cout << "Hello world" << endl;
+  cout << "Hello worldasdfkadsjfhadskjfhasdkj" << endl;
 
+  cout << "Hello world" << endl;
+  int a  = 10;
+  cout << a << endl;
   return 0;
 }
